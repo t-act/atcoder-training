d4954e89c5411ff9f6683500baddb6c5ae8aa281
t-act <takuto.505521@gmail.com>
2026-07-04 14:24:28 +0900

    abc007: C retry

---
+++ src/abc007_c.py
@@ -0,0 +1,46 @@
+# https://atcoder.jp/contests/abc007/tasks/abc007_3
+from collections import deque
+
+r, c = map(int, input().split())
+start = tuple(x - 1 for x in map(int, input().split()))
+goal = tuple(x - 1 for x in map(int, input().split()))
+
+c = []
+for rr in range(r):
+    grid = list(input())
+    c.append(grid)
+
+
+def bfs(maze, start, goal):
+    rows, cols = len(maze), len(maze[0])
+
+    visited = {start}       # 訪問したかを記録
+    que = deque([start])
+    parent = {start: None}  # 各セルをどのセルから発見したかを記録(key: 移動後, value: 移動前)
+
+    while que:
+        r, c = que.popleft()
+
+        # goalか確認
+        if (r, c) == goal:
+            path = []
+            node = (r, c)
+            while node is not None:
+                path.append(node)
+                node = parent[node]
+            return path[::-1]
+
+        # 上下左右を確認を確認して進めるならばをqueueにpush
+        for dr, dc in [(-1,0), (1,0), (0,-1), (0,1)]:
+            nr, nc = r+dr, c+dc  # 移動後
+            if (0 <= nr < rows and 0<= nc < cols   # grid範囲内か
+                    and maze[nr][nc] != "#"         # 壁でないか
+                    and (nr, nc) not in visited):   # 未訪問か
+                # 発見したらvisitedに登録する
+                visited.add((nr, nc))
+                parent[(nr, nc)] = (r, c)  # key: 移動後, value: 移動前
+                que.append((nr, nc))
+    return None
+
+ans = bfs(c, start, goal)
+print(len(ans)-1)
